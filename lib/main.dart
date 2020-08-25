983b0ae537607bcc35244a24338f6d897402937f
hideyuk1-jp <hh11028+github@gmail.com>
2020-08-25 11:57:21 +0900

    デザイン修正、フォームでdue_dateの投稿機能実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_todolist_app/sharedPref.dart';
+import 'package:intl/intl.dart';
 import 'task.dart';
 
 void main() {
@@ -16,15 +17,13 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: TaskPage(title: 'Flutter TODO'),
+      home: TaskPage(),
     );
   }
 }
 
 class TaskPage extends StatefulWidget {
-  TaskPage({Key key, this.title}) : super(key: key);
-
-  final String title;
+  TaskPage({Key key}) : super(key: key);
 
   @override
   _TaskPageState createState() => _TaskPageState();
@@ -34,9 +33,12 @@ class _TaskPageState extends State<TaskPage> with TickerProviderStateMixin {
   List<Task> _tasks = <Task>[];
   List<Task> _completedTasks = <Task>[];
   List<Task> _incompletedTasks = <Task>[];
+
   final _textController = TextEditingController();
   final FocusNode _focusNode = FocusNode();
   bool _isComposing = false;
+  DateTime _dueDate = new DateTime.now();
+  // int _estimatedMinutes = 5;
 
   SharedPref sharedPref = SharedPref();
 
@@ -51,6 +53,29 @@ class _TaskPageState extends State<TaskPage> with TickerProviderStateMixin {
     });
   }
 
+  Future<Null> _selectDueDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: _dueDate,
+        firstDate: new DateTime(2016),
+        lastDate: new DateTime.now().add(new Duration(days: 360)));
+    if (picked != null) setState(() => _dueDate = picked);
+  }
+
+  String _dateFormatter(DateTime date) {
+    DateTime today = new DateTime.now();
+    Map<int, String> conv = {-1: '昨日', 0: '今日', 1: '明日', 2: '明後日'};
+    for (int key in conv.keys) {
+      DateTime cdate = today.add(new Duration(days: key));
+      print(DateFormat('yyyy年M月d日').format(cdate));
+      if (date.year == cdate.year &&
+          date.month == cdate.month &&
+          date.day == cdate.day) return conv[key];
+    }
+    if (today.year == date.year) return DateFormat('M月d日').format(date);
+    return DateFormat('yyyy年M月d日').format(date);
+  }
+
   @override
   void initState() {
     super.initState();
@@ -61,47 +86,75 @@ class _TaskPageState extends State<TaskPage> with TickerProviderStateMixin {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Center(
+          child: Icon(Icons.style),
+        ),
       ),
       body: Container(
         child: Column(
           children: <Widget>[
-            Flexible(
-              child: ListView.builder(
-                padding: EdgeInsets.all(8.0),
-                itemBuilder: (_, int index) {
-                  return Container(
+            Container(
+              padding: EdgeInsets.only(
+                  top: 16.0, right: 12.0, bottom: 16.0, left: 12.0),
+              child: Container(
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: Text(
+                    '今日 : 8月24日(月)',
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                padding: EdgeInsets.zero,
+                children: _incompletedTasks.map((Task task) {
+                  return Card(
+                    key: Key(task.uuid),
                     child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Container(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.only(top: 1.0),
                           child: IconButton(
-                            padding: EdgeInsets.zero,
-                            icon: Icon(
-                                _incompletedTasks[index].completedAt == null
-                                    ? Icons.check_box_outline_blank
-                                    : Icons.check_box),
-                            onPressed: () =>
-                                _handleCompleted(_incompletedTasks[index]),
+                            icon: Icon(task.completedAt == null
+                                ? Icons.radio_button_unchecked
+                                : Icons.check_box),
+                            onPressed: () => _handleCompleted(task),
+                            enableFeedback: false,
                           ),
                         ),
                         Expanded(
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              //Text(_name, style: Theme.of(context).textTheme.headline4),
-                              Container(
-                                margin: EdgeInsets.all(12.0),
-                                child: Text(_incompletedTasks[index].text),
-                              ),
-                            ],
+                          child: Padding(
+                            padding: EdgeInsets.only(
+                                top: 12.0, right: 12.0, bottom: 12.0),
+                            child: Text(
+                              task.text,
+                              style: TextStyle(fontSize: 16),
+                            ),
                           ),
                         ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 1.0),
+                          child: IconButton(
+                            icon: Icon(Icons.more_vert),
+                            onPressed: () => print('Press!'),
+                            enableFeedback: false,
+                          ),
+                        ),
+                        // タップでEタスクのdit画面へ
                       ],
                     ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                    ),
                   );
-                },
-                itemCount: _incompletedTasks.length,
+                }).toList(),
               ),
             ),
           ],
@@ -113,7 +166,7 @@ class _TaskPageState extends State<TaskPage> with TickerProviderStateMixin {
             isScrollControlled: true,
             context: context,
             shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
+              borderRadius: BorderRadius.vertical(top: Radius.circular(8.0)),
             ),
             builder: (BuildContext context) {
               return StatefulBuilder(builder: (BuildContext context, setState) {
@@ -123,35 +176,89 @@ class _TaskPageState extends State<TaskPage> with TickerProviderStateMixin {
                     child: Container(
                       padding: EdgeInsets.only(
                           bottom: MediaQuery.of(context).viewInsets.bottom),
-                      child: Row(
+                      child: Column(
                         children: <Widget>[
-                          Flexible(
-                            child: TextField(
-                              controller: _textController,
-                              onChanged: (String text) {
-                                setState(() {
-                                  _isComposing = text.length > 0;
-                                });
-                              },
-                              onSubmitted: _isComposing
-                                  ? (String text) =>
-                                      _handleSubmitted(text, context)
-                                  : null,
-                              autofocus: true,
-                              decoration:
-                                  InputDecoration.collapsed(hintText: 'タスクを追加'),
-                              focusNode: _focusNode,
-                            ),
+                          TextField(
+                            controller: _textController,
+                            onChanged: (String text) {
+                              setState(() {
+                                _isComposing = text.length > 0;
+                              });
+                            },
+                            onSubmitted: _isComposing
+                                ? (String text) =>
+                                    _handleSubmitted(text, _dueDate, context)
+                                : null,
+                            autofocus: true,
+                            decoration:
+                                InputDecoration.collapsed(hintText: 'タスクを追加'),
+                            focusNode: _focusNode,
                           ),
-                          Container(
-                            child: IconButton(
-                              icon: const Icon(Icons.send),
-                              color: Colors.blue,
-                              onPressed: _isComposing
-                                  ? () => _handleSubmitted(
-                                      _textController.text, context)
-                                  : null,
-                            ),
+                          Padding(
+                            padding: EdgeInsets.only(bottom: 8.0),
+                          ),
+                          Row(
+                            children: <Widget>[
+                              Container(
+                                child: OutlineButton.icon(
+                                  icon: Icon(
+                                    Icons.calendar_today,
+                                    color: Colors.green,
+                                    size: 16.0,
+                                  ),
+                                  label: Text(
+                                    _dateFormatter(_dueDate),
+                                    //DateFormat('yyyy年M月d日').format(_dueDate),
+                                    style: TextStyle(
+                                      color: Colors.green,
+                                      fontSize: 14,
+                                      letterSpacing: -0.8,
+                                    ),
+                                  ),
+                                  onPressed: () => _selectDueDate(context),
+                                  color: Colors.green,
+                                  shape: OutlineInputBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(8.0)),
+                                  ),
+                                ),
+                              ),
+                              /*Padding(padding: EdgeInsets.only(right: 8.0)),
+                              Container(
+                                child: OutlineButton.icon(
+                                  icon: Icon(
+                                    Icons.timer,
+                                    size: 16.0,
+                                  ),
+                                  label: Text(
+                                    "時間なし",
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      letterSpacing: -0.8,
+                                    ),
+                                  ),
+                                  onPressed: () {},
+                                  color: Colors.green,
+                                  shape: OutlineInputBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(8.0)),
+                                  ),
+                                ),
+                              ),*/
+                              Spacer(),
+                              Container(
+                                child: IconButton(
+                                  icon: const Icon(Icons.send),
+                                  color: Colors.blue,
+                                  onPressed: _isComposing
+                                      ? () => _handleSubmitted(
+                                          _textController.text,
+                                          _dueDate,
+                                          context)
+                                      : null,
+                                ),
+                              ),
+                            ],
                           ),
                         ],
                       ),
@@ -168,12 +275,15 @@ class _TaskPageState extends State<TaskPage> with TickerProviderStateMixin {
     );
   }
 
-  void _handleSubmitted(String text, BuildContext context) async {
+  void _handleSubmitted(
+      String text, DateTime dueDate, BuildContext context) async {
     _textController.clear();
     setState(() {
       _isComposing = false;
+      _dueDate = new DateTime.now();
     });
-    Task task = Task.fromMap({'text': text});
+    Task task = Task.fromMap(
+        {'text': text, 'due_date': DateFormat('yyyy-MM-dd').format(dueDate)});
     await sharedPref.createTask(task);
     _focusNode.requestFocus();
     _loadTasks();
